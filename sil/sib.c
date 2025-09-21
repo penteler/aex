dea84ee48fdbae21695e4f72b71fbe30397e51e8
penteler <impromooray@gmail.com>
2025-09-22 01:35:55 +0530

    x

--- sil/sib.c
+++ sil/sib.c
@@ -6,11 +6,26 @@ int main() {
   char theSt[] = " 1234,56.789?0l ";
   int pos = strcspn(theSt, ",.?l");
   printf("without strcspn: %s as a whole\n ", theSt);
+  theSt[8] = '\0';
+  printf("\n 8 characters %s", theSt);
+
+  theSt[strcspn(theSt, "l")] = '\0';
+  printf("\n upto the l %s", theSt);
+
   theSt[strcspn(theSt, ",")] = '\0';
   printf("with %s trimed\n", theSt);
+  theSt[6] = '\0';
+  printf("with 6 charaters %s", theSt);
+  theSt[strcspn(theSt, ".")] = '\0';
+  printf("to the dot %s", theSt);
 
   printf("%d", pos);
   return 0;
 }
+/*
+without strcspn:  1234,56.789?0l  as a whole
 
-
+ 8 characters  1234,56
+ upto the l  1234,56with  1234 trimed
+with 6 charaters  1234to the dot  12345
+*/
---
+++ sil/ttra.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define filename "Trainers.dat"
+
+struct Trainer {
+    int ID;
+    float salary;
+    char name[50];
+    char dateofjoining[20];
+    char department[59];
+    char position[40];
+};
+
+void createRecord(struct Trainer trainers[], int *count);
+void listRecord(struct Trainer trainers[], int count);
+void modifyRecord(struct Trainer trainers[], int count);
+void deleteRecord(struct Trainer trainers[], int *count);
+
+int main() {
+    struct Trainer trainers[100];
+    int count = 0;
+    int choice;
+    do {
+	printf("\n == Trainers database == \n");
+	printf("\n 1. Create a record \n");
+	printf("2. List records \n");
+	printf("3. Modify a record \n");
+	printf("4. Delete a record \n");
+	printf("5. Exit \n");
+	printf("Pick an option: ");
+	scanf("%d" &choice );
+	switch(choice) {
+	    case 1:
+		createRecord(trainers, &count);
+		break;
+	    case 2:
+		listRecords(trainers, count);
+		break;
+	    case 3:
+		modifyRecord(trainers, count);
+		break;
+	    case 4:
+		deleteRecord(trainers, &count);
+		break;
+	    case 5:
+		printf("\n Exiting..");
+		break;
+	    default:
+		printf("\n Not a valid choice");
+	}
+    } while(choice != 5);
+    return 0;
+}
+
+void createRecord(struct Trainer trainers[], int *count) {
+    printf("\n Enter trainer ID: ");
+    scanf("%d", &trainers[*count].trainerID);
+    getchar();
+
+    printf("\n Enter the name of trainer: ");
+    fgets(trainers[*count].name, sizeof(trainers[*count].name), stdin);
+    trainers[*count].name[strcspn(trainers[*count].name, "\n")] = '\0';
+
+    printf("\n Name of the Department: ");
+    fgets(trainers[*count].department, sizeof(trainers[*count].), stdin);
+    trainers[*count].department[strcspn(trainers[*count].department, "\n")] = '\0';
+
+    printf("\n position: ");
+    fgets(trainers[*count].position, sizeof(trainers[*count].position), stdin);
+    trainers[*count].positon[strcspn(trainers[*count].position, "\n")] = '\0';
+
+    printf("\n Date of joining: ");
+    fgets(trainers[*count].dateofjoining, sizeof(trainers[*count].dateofjoining), stdin);
+    trainers[*count].dateofjoining[strcspn(trainers[*count].dateofjoining, "\n")] = '\0';
+
+    printf("\n Salary: ");
+    scanf("%f", &trainers[*count].salary);
+
+    (*count)++;
+    printf("Your info has been added sucessfully!");
+}
+
+void listRecords(sturct Trainer[], int count) {
+    if (count ==0) {
+	printf("\n no records found! \n");
+	return;
+    }
+
+    printf("\n=== List of Trainers ===\n");
+    for (int i = 0; i < count; i++) {
+	printf("\nTrainer ID: %d" trainers[i].trainerID);
+	printf("\nName: %s", trainers[i].name);
+	printf("\nDepartment: %s", trainers[i].department);
+	printf("\nPosition: %s", trainers[i].position);
+	printf("\nDate of joining: %s", trainers[i].dateofjoining);
+	printf("\nSalary: %.2f\n", trainers[i].salary);
+	printf("--------------------------------\n");
+    }
+}
+
+void modifyRecord(struct Trainer trainers[], int count) {
+    int id, found = 0;
+    printf("\nEnter Trainer ID to modify: ");
+    scanf("%d", &id);
+    getchar();
+    for (int i = 0; i < count; i++) {
+	if (trainers[i].trainerID == id) {
+	    found = 1;
+
+	    printf("Enter new Trainer Name: ");
+	    fgets(trainer[i].name, sizeof(trainer[i].name), stdin);
+	    trainers[i].name[strcspn(trainers[i].name, "\n")] = '\0';
+	    
+	    printf("Department: ");
+	    fgets(trainer[i].department, sizeof(trainer[i].department, stdin);
+	    trainers[i].department[strcspn(trainers[i].department, "\n")] = '\0';
+
+	    printf("Position: ");
+            fgets(trainer[i].position, sizeof(trainer[i].position, stdin);
+            trainers[i].position[strcspn(trainers[i].position, "\n")] = '\0';
+
+	    printf("Date of joining");
+            fgets(trainer[i].dateofjoining, sizeof(trainer[i].dateofjoining, stdin);
+            trainers[i].dateofjoining[strcspn(trainers[i].dateofjoining, "\n")] = '\0';
+
+	    printf("Salary: ");
+	    scanf("%f", &trainters[i].salary);
+
+	    printf("The record has been updated");
+	    break;
+	}
+    }
+
+    if (!found)
+    	printf("%d is not a valid ID\n", id);
+}
+
+void deleteRecord(struct Trainer trainers[], int *count) {
+    int id, found = o;
+    printf("Enter trainer ID: ");
+    scanf("%d", &id);
+
+    for (int i = 0; i < *count; i++) {
+	if (trainers[i].trainerID == id) {
+	    found = 1;
+	    for (int j = i; j < (*count) - 1; j++) {
+	        trainers[j] = trainers[j + 1];
+	    }
+	    (*count)--;
+	    printf("Record deleted successfully!\n");
+	    break;
+	}
+    }
+    if (!found)
+	printf("%d is an invalid ID\n", id);
+}
+
+
+
+
+
+		
