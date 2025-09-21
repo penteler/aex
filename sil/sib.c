fe68cc54c01d483823894a5584bb790179ce613f
penteler <impromooray@gmail.com>
2025-09-21 20:28:04 +0530

    x

---
+++ sil/sib.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char thet[77];
+  char theSt[] = " 1234,56.789?0l ";
+  int pos = strcspn(theSt, ",.?l");
+  printf("without strcspn: %s as a whole\n ", theSt);
+  theSt[strcspn(theSt, ",")] = '\0';
+  printf("with %s trimed\n", theSt);
+
+  printf("%d", pos);
+  return 0;
+}
+
+
